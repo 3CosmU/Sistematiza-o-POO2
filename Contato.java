e853b3afb8e275d09d8155128921b02ad3166091
Wagner Moreira <140017065+3CosmU@users.noreply.github.com>
2025-04-13 21:26:37 -0300

    Contato.java

--- Contato.java
+++ Contato.java
@@ -2,15 +2,13 @@ class Contato {
     String nome;
     String numeroTelefone;
     String email;
-
-    // Construtor para criar um novo contato
+  
     public Contato(String nome, String numeroTelefone, String email) {
         this.nome = nome;
         this.numeroTelefone = numeroTelefone;
         this.email = email;
     }
 
-    // Método para mostrar as informações do contato como texto
     @Override
     public String toString() {
         return "Nome: " + nome + ", Telefone: " + numeroTelefone + ", Email: " + email;
